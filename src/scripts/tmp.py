02f083d5c0ef6aea579c3460d69154bfff935ed4
Your Name <you@example.com>
2022-09-05 15:38:17 -0400

    asdf

---
+++ src/scripts/tmp.py
@@ -0,0 +1,10 @@
+a=[1,2,3]
+print(f'a = [1, 2, 3].')
+b=a
+print(f'b = a.')
+print(f'b == a is {b==a}.')
+print(f'b is a is {b is a}.')
+c=a[:]
+print(f'c = a[:].')
+print(f'c == a is {c==a}.')
+print(f'c is a is {c is a}.')
